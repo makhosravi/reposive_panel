db98ce2fe6eccbb243adc9b7f08ba3fffae9e5b5
makhosravi <peter_stark95@yahoo.com>
2022-03-15 23:31:19 +0330

    implemented widget tree & responsive handler

---
+++ flutter_responsive_panel/lib/widget_tree.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_responsive_panel/responsive_layout.dart';
+
+class WidgetTree extends StatefulWidget {
+  const WidgetTree({Key? key}) : super(key: key);
+
+  @override
+  State<WidgetTree> createState() => _WidgetTreeState();
+}
+
+class _WidgetTreeState extends State<WidgetTree> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+          child: (ResponsiveLayout.isTinyLimit(context) ||
+                  ResponsiveLayout.isTinyHeightLimit(context)
+              ? Container()
+              : AppBar()),
+          preferredSize: const Size(double.infinity, 100.0)),
+      body: ResponsiveLayout(
+        tiny: Container(),
+        phone: Container(),
+        tablet: Container(),
+        largeTablet: Container(),
+        computer: Container(),
+      ),
+    );
+  }
+}

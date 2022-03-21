f64daf95ba7312ff628bdc8b650c516b8bdb50a1
makhosravi <peter_stark95@yahoo.com>
2022-03-21 22:32:37 +0330

    implemented center panel page

--- flutter_responsive_panel/lib/panel_center/panel_center_page.dart
+++ flutter_responsive_panel/lib/panel_center/panel_center_page.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_responsive_panel/constants.dart';
+import 'package:flutter_responsive_panel/panel_center/linear_chart.dart';
+
+class Person {
+  Person({required this.name, required this.color});
+  final String name;
+  final Color color;
+}
 
 class PanelCenterPage extends StatefulWidget {
   const PanelCenterPage({Key? key}) : super(key: key);
@@ -8,11 +16,84 @@ class PanelCenterPage extends StatefulWidget {
 }
 
 class _PanelCenterPageState extends State<PanelCenterPage> {
+  final List<Person> _persons = [
+    Person(name: 'Theia Bowen', color: Constants.orangeLight),
+    Person(name: 'Fariha Odling', color: Constants.redLight),
+    Person(name: 'Viola Willis', color: Constants.blueLight),
+    Person(name: 'Emmet Forrest', color: Constants.orangeLight),
+    Person(name: 'Nick Jarvis', color: Constants.greenLight),
+    Person(name: 'Amit Claveia', color: Constants.orangeLight),
+    Person(name: 'Amalie Howardeia', color: Constants.redLight),
+    Person(name: 'Campbell Britton', color: Constants.blueLight),
+    Person(name: 'Harley Mellor', color: Constants.redLight),
+    Person(name: 'Harlen Higgins', color: Constants.greenLight),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                left: Constants.kPadding / 2,
+                right: Constants.kPadding / 2,
+                top: Constants.kPadding / 2,
+              ),
+              child: Card(
+                elevation: 3.0,
+                color: Constants.purpleLight,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50.0),
+                ),
+                child: const SizedBox(
+                  width: double.infinity,
+                  child: ListTile(
+                    title: Text(
+                      'Products Available',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    subtitle: Text(
+                      '82& of Products Avail.',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    trailing: Chip(
+                      label: Text(
+                        '20.500',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const BarChartSample2(),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: Constants.kPadding / 2,
+                right: Constants.kPadding / 2,
+                top: Constants.kPadding,
+                bottom: Constants.kPadding,
+              ),
+              child: Card(
+                elevation: 3.0,
+                color: Constants.purpleLight,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0),),
+                child: Column(
+                  children: List.generate(_persons.length, (index) => ListTile(
+                    leading: CircleAvatar(
+                      radius: 15.0,
+                      child: Text(_persons[index].name.substring(0, 1), style: const TextStyle(color: Colors.white),),
+                      backgroundColor: _persons[index].color,
+                    ),
+                    title: Text(_persons[index].name, style: const TextStyle(color: Colors.white),),
+                    trailing: IconButton(onPressed: (){}, icon: const Icon(Icons.message, color: Colors.white,),),
+                  )),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }

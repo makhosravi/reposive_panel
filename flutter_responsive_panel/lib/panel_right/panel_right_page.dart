74aa851b7955dc3b05e88e08a0fbf5ff46e1ecf4
makhosravi <peter_stark95@yahoo.com>
2022-03-21 23:07:18 +0330

    implemented right panel page

--- flutter_responsive_panel/lib/panel_right/panel_right_page.dart
+++ flutter_responsive_panel/lib/panel_right/panel_right_page.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_responsive_panel/constants.dart';
+import 'package:flutter_responsive_panel/panel_right/wiggle_gragh.dart';
+
+class Product {
+  Product({required this.name, this.enable = true});
+
+  final String name;
+  bool enable;
+}
 
 class PanelRightPage extends StatefulWidget {
   const PanelRightPage({Key? key}) : super(key: key);
@@ -8,11 +17,90 @@ class PanelRightPage extends StatefulWidget {
 }
 
 class _PanelRightPageState extends State<PanelRightPage> {
+  final List<Product> _products = [
+    Product(name: 'LED Submersible light', enable: true),
+    Product(name: 'Portable Projector', enable: true),
+    Product(name: 'Bluetooth Speaker', enable: true),
+    Product(name: 'Smart Watch', enable: true),
+    Product(name: 'Temporary Tattoos', enable: true),
+    Product(name: 'Bookends', enable: true),
+    Product(name: 'Vegtable Chopper', enable: true),
+    Product(name: 'Neck Messager', enable: true),
+    Product(name: 'Facial Cleanser', enable: true),
+    Product(name: 'Back Cushion', enable: true),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                left: Constants.kPadding / 2,
+                right: Constants.kPadding / 2,
+                top: Constants.kPadding / 2,
+              ),
+              child: Card(
+                elevation: 3.0,
+                color: Constants.purpleLight,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50.0),
+                ),
+                child: const SizedBox(
+                  width: double.infinity,
+                  child: ListTile(
+                    title: Text(
+                      'Net Revenue',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    subtitle: Text(
+                      '7% of Saves Avg.',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    trailing: Chip(
+                      label: Text(
+                        r'$46,450',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const LineChartSample1(),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: Constants.kPadding / 2,
+                right: Constants.kPadding / 2,
+                top: Constants.kPadding,
+                bottom: Constants.kPadding,
+              ),
+              child: Card(
+                elevation: 3.0,
+                color: Constants.purpleLight,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: Column(
+                  children: List.generate(
+                    _products.length,
+                    (index) => SwitchListTile.adaptive(
+                      title: Text(_products[index].name, style: const TextStyle(color: Colors.white),),
+                      value: _products[index].enable,
+                      onChanged: (newValue){
+                        setState(() {
+                          _products[index].enable = newValue;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
